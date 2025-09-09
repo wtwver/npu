31f804918d50051766ce12142d9cef4d6d7f8780
wtwver <a@a.com>
2025-09-09 15:07:33 +0800

    add cpp

---
+++ cpp/build/CMakeFiles/int32add.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/int32add.dir/int32add.cpp.o"
+  "CMakeFiles/int32add.dir/int32add.cpp.o.d"
+  "int32add"
+  "int32add.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/int32add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
